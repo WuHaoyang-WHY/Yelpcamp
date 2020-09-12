0eb99f2281a42afe13b042a412696de744174bf5
Haoyang Wu <wuhaoyang919@gmail.com>
2020-09-12 15:32:15 +0800

    Added basic frame for web

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 port = 3000;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+var campgrounds = [
+    {name: 'Haoyang', image:"https://placekitten.com/g/400/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/300/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/500/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/200/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/400/300"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/400/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/400/200"},
+    {name: 'Haoyang', image:"https://placekitten.com/g/400/200"},
+    {name: 'user2', image:"https://placekitten.com/g/400/200"}
+];
 
 app.get("/", function (req, res) {
     res.render("landing");
@@ -10,13 +24,25 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [
-        {name: 'Haoyang', image:"https://placekitten.com/g/200/300"},
-        {name: 'user2', image:""}
-    ];
 
     res.render('campgrounds',{campgrounds:campgrounds});
 });
+
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newcampground = {name: name, image:image};
+    campgrounds.push(newcampground);
+
+    res.redirect("/campgrounds");
+});
+
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new.ejs");
+});
+
+
 
 
 app.listen(port, () => {
